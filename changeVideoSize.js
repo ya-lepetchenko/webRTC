be75c36af1ef6df87314437c2f04a9b34d0b6afd
Yaroslav <ya.lepetchenko@onepointcollab.com>
2024-03-07 16:39:38 +0200

    ADD: change video size

--- changeVideoSize.js
+++ changeVideoSize.js
@@ -4,18 +4,20 @@ console.log(supportCOnstraints)
 
 const changeVideoSize = (e) => {
     //stream.getTracks().forEach(track => {
-        const c = track.getCapabilities()
-        const height = document.querySelector('#vid-height').value
-        const width = document.querySelector('#vid-with').value
+        const height = document.querySelector('#vid-height').nodeValue
+        const width = document.querySelector('#vid-with').nodeValue
         stream.getVideoTracks().forEach(track => {
-            const vConstraints = {
-                height: {exact: height < c.height.max ? height : c.height.max},
-                width: {exact: width < c.width.max ? width : c.width.max},
-                //frameRate: 5
-            }
-            track.applyConstraints(vConstraints)
+            if(track) {
+                const c = track.getCapabilities()
+                const vConstraints = {
+                    height: {exact: height < c.height.max ? height : c.height.max},
+                    width: {exact: width < c.width.max ? width : c.width.max},
+                    //frameRate: 5
+                }
+                track.applyConstraints(vConstraints)
+            } 
         })
+    }
         // const c = track.getCapabilities()
         // console.log(c)
     //})
-}
