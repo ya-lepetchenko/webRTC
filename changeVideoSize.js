a35c6f83814446e7aab9737cf09790e535717ded
Yaroslav <ya.lepetchenko@onepointcollab.com>
2024-03-07 15:18:47 +0200

    ADD: lesson 9 completed - added video capabilities switching

--- changeVideoSize.js
+++ changeVideoSize.js
@@ -3,8 +3,19 @@ const supportCOnstraints = navigator.mediaDevices.getSupportedConstraints()
 console.log(supportCOnstraints)
 
 const changeVideoSize = (e) => {
-    stream.getTracks().forEach(track => {
+    //stream.getTracks().forEach(track => {
         const c = track.getCapabilities()
-        console.log(c)
-    })
+        const height = document.querySelector('#vid-height').value
+        const width = document.querySelector('#vid-with').value
+        stream.getVideoTracks().forEach(track => {
+            const vConstraints = {
+                height: {exact: height < c.height.max ? height : c.height.max},
+                width: {exact: width < c.width.max ? width : c.width.max},
+                //frameRate: 5
+            }
+            track.applyConstraints(vConstraints)
+        })
+        // const c = track.getCapabilities()
+        // console.log(c)
+    //})
 }
